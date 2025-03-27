ab57f75eba0d73966aa2d7b012e90c5a99fbc797
morison <frederickmorison@gmail.com>
2025-03-27 02:41:42 +0000

    features added

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,18 +1,25 @@
-# Uncomment the imports before you add the code
-# from django.urls import path
+from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-# from . import views
+from . import views
+from django.views.generic import TemplateView
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # # path for registration
+    # path for registration
+        path(route='register',view= views.registration, name='register'),
 
     # path for login
-    # path(route='login', view=views.login_user, name='login'),
+        path(route='login', view=views.login_user, name='login'),
+        path(route='logout',view=views.logout_request,name='logout'),
+        path(route='get_cars', view=views.get_cars, name ='getcars'),
 
     # path for dealer reviews view
+    #path('reviews/', views.dealer_reviews, name='reviews'),
 
     # path for add a review view
+    #path('add_review/', views.add_review, name='add_review'),
 
+    # path for the index page (if needed)
+    #path('index/', TemplateView.as_view(template_name="index.html"), name='index'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
