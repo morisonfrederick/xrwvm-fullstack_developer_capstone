984d86ed1a96526b09735cf9fbcc065f33dbf6ed
morison <frederickmorison@gmail.com>
2025-03-27 05:10:48 +0000

    changes

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,7 +1,7 @@
 # Uncomment the following imports before adding the Model code
 
 from django.db import models
-from django.utils.timezone import now
+#from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -15,10 +15,9 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
+
     def __str__(self):
         return self.name
-
-
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many
@@ -29,16 +28,15 @@ class CarMake(models.Model):
 # - Year (IntegerField) with min value 2015 and max value 2023
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
-
 class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake,on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     CAR_TYPES =[
         ('SEDAN','Sedan'),
         ('SUV','SUV'),
-        ('WAGON','Wagon')
+        ('WAGON','Wagon'),
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    year = models.IntegerField(default=2025,validators=[MaxValueValidator(2025),MinValueValidator(2011)])
+    year = models.IntegerField(default=2025, validators=[MaxValueValidator(2025), MinValueValidator(2011)])
     def __str__(self):
         return self.name
